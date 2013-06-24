6eeed40f584b2a16f6f562d95bec508fb10b0b80
João Almeida <joao.diogo.almeida@gmail.com>
2013-06-24 18:38:34 +0100

    SE HW1 P2

---
+++ sehw1p2.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+var start = new Date();
+var fs = require('fs');
+var outfile = "sehw1.txt";
+
+var out;
+
+for (var i = 2; i < 500; i++)
+{
+	for (var j = 2; j < i; j++)
+	{
+		if (i % j == 0)
+		{
+			var prime = 'no';
+			break;
+		}
+		else
+			var prime = 'yes';
+	}
+	if (prime == 'yes')
+	{
+		out += i + ",";
+	}
+}							
+
+fs.writeFileSync(outfile, out);
+var end = new Date() - start;
+console.log("Script took " + end);
+
